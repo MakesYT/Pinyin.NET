3d36fcf3bfca7f3c83c64e0727faf421a4838be1
MakesYT <42534870+MakesYT@users.noreply.github.com>
2024-04-17 01:16:08 +0900

    1

--- Pinyin.NET/PinyinSearcher.cs
+++ Pinyin.NET/PinyinSearcher.cs
@@ -5,6 +5,7 @@ namespace Pinyin.NET;
 
 public class PinyinSearcher
 {
+    
     public IEnumerable Source { get; set; }
     public string PropertyName { get; set; }
     public PinyinSearcher(IEnumerable source, string propertyName)
@@ -115,4 +116,136 @@ public class PinyinSearcher
     }
    
   
+}
+
+public class PinyinSearcher<T> 
+{
+    public Dictionary<object,T> SourceDictionary { get; set; }
+    public IEnumerable Source { get; set; }
+    public string PropertyName { get; set; }
+    public PinyinSearcher(IEnumerable source, string propertyName)
+    {
+        Source = source;
+        PropertyName = propertyName;
+        
+    }
+    public PinyinSearcher(Dictionary<object,T> source, string propertyName)
+    {
+        SourceDictionary = source;
+        PropertyName = propertyName;
+        
+    }
+    
+    public IEnumerable<SearchResults<T>> Search(string query)
+    {
+        var results = new List<SearchResults<T>>();
+        if (Source is null)
+        {
+            if (SourceDictionary is not null)
+            {
+                Source = SourceDictionary.Values;
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(Source));
+            }
+            
+        }
+        foreach (var x in Source)
+        {
+            var value = x.GetType()
+                         .GetProperty(PropertyName)
+                         .GetValue(x);
+            if (value
+                is IEnumerable<IEnumerable<string>> pinyins )
+            {
+                
+                var matches =new bool[pinyins.Count()];
+                query = query.ToLower();
+                var weight = 0;
+                var matchIndexList = new List<int>();
+                var matchT = true;
+                //检测搜索词的每一个字符
+                for (var i = 0; i < query.Length; i++)
+                {
+                    //Console.WriteLine("搜索字符"+query[i]);
+                    var array = matchIndexList.ToArray();
+                    if (array.Length == 0)
+                    {
+                        array = new int[]{0};
+                    }
+                    var allMatchNum =array.Length;
+                    matchIndexList.Clear();
+                    var match = false;
+                    foreach (var i1 in array)
+                    {
+                        for (int j = i1; j < pinyins.Count(); j++)
+                        {
+                            //Console.WriteLine(" 搜索拼音"+string.Join(",",pinyins.ElementAt(j)));
+                            var elementAt = pinyins.ElementAt(j);
+                            foreach (var se in elementAt)
+                            {
+                                //Console.WriteLine("     拼音字符"+se);
+                                if (se.StartsWith(query[i]))
+                                {
+                                    //Console.WriteLine("     匹配成功");
+                                    StringBuilder sb = new();
+                                    sb.Append(query[i]);
+                                    while (i < query.Length - 1)
+                                    {
+                                        sb.Append(query[i+1]);
+                                        if (!se.StartsWith(sb.ToString()))
+                                        {
+                                            //Console.WriteLine("     "+sb+" "+se+"匹配失败");
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            //Console.WriteLine("     "+sb+" "+se+"匹配成功");
+                                            i++;
+                                        }
+                                    }
+                                    //Console.WriteLine("     L "+sb+" "+se+"匹配成功");
+                                    match = true;
+                                    matches[j] = true;
+                                    weight++;
+                                    if (!matchIndexList.Contains(j)&&j>i1)
+                                    {
+                                        matchIndexList.Add(j);
+                                    }
+                                    
+                                
+                                }
+                            }
+                        }
+                        if (!match)
+                        {
+                            --allMatchNum;
+                            break;
+                        }
+                    }
+                    if (allMatchNum == 0)
+                    {
+                        matchT = false;
+                        break;
+                    }
+                    
+                   
+                }
+                if (matchT)
+                {
+                    results.Add(new SearchResults<T>
+                    {
+                        Source = (T)x,
+                        Weight = weight,
+                        CharMatchResults = matches
+                    });
+                }
+
+                
+            }
+        }
+       
+        return results;
+    }
 }
--- Pinyin.NET/SearchResults.cs
+++ Pinyin.NET/SearchResults.cs
@@ -1,8 +1,13 @@
 ﻿namespace Pinyin.NET;
-
-public struct SearchResults
+public struct SearchResults 
 {
     public object Source { get; set; }
+    public int Weight { get; set; }
+    public bool[] CharMatchResults { get; set; }
+}
+public struct SearchResults<T> 
+{
+    public T Source { get; set; }
     public int Weight { get; set; }
     public bool[] CharMatchResults { get; set; }
 }
